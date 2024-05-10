6cad69f136969ee1fabf3da372199a1b918ae08d
Klaus.Nascimento <klausnascimento@gmail.com>
2024-05-09 22:47:02 -0300

    Este código React define um componente de exemplo que representa um carrinho de compras. Ele inclui um botão para abrir e fechar um diálogo que exibe os produtos no carrinho. O diálogo é implementado com a biblioteca @headlessui/react, usando transições para animações suaves de entrada e saída. Cada produto é exibido com seu nome, preço, cor, quantidade e um vídeo incorporado do YouTube. Os produtos são mapeados de uma matriz estática chamada products. O subtotal do carrinho é exibido no rodapé do diálogo, juntamente com opções para finalizar a compra ou continuar comprando. O componente utiliza o estado local para controlar se o diálogo está aberto ou fechado.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ModalHome from "@/components/Modal/ModalHome";
+import ShoppingCarts from "@/components/SlideOvers/ShoppingCarts";
 import SlideOvers from "@/components/SlideOvers/SlideOvers";
 
 export default function Home() {
   return (
     <>
     <ModalHome />
-    <SlideOvers />
+    {/* <SlideOvers /> */}
+    <ShoppingCarts />
     </>
   );
 }
